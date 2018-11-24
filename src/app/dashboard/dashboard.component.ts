321a0a9d1534053b452cb99709db3dc911c8ffbf
Princeton Evans <princetonevans@yahoo.com>
2018-11-24 12:40:42 -0600

    removed another console.log

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -64,7 +64,7 @@ export class DashboardComponent implements OnInit {
       this.customerDataSource.sort = this.customerSort;
 
       this.user = this.authService.user;
-      console.log(this.user)
+
   }
 
 
