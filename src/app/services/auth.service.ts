9b925ea7150de56ea044ba312a31b0fe681619b7
Princeton Evans <princetonevans@yahoo.com>
2019-01-03 19:58:31 -0600

    centered footer, changed nav and changed homepage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,74 +8,61 @@ import { Router } from '@angular/router';
 export class AuthService {
 
   apiUrl = 'localhost:3000';
-  user;
+  user = firebase.auth().currentUser;
 
   constructor(private http: HttpClient,
               private router: Router) { }
 
-  // login(username: string, password: string) {
-  //     return this.http.post<any>(this.apiUrl,  {username, password })
-  //         .pipe(map(user => {
-  //             // login successful if there's a user in the response
-  //             if (user) {
-  //                 // store user details and basic auth credentials in local storage
-  //                 // to keep user logged in between page refreshes
-  //                 user.authdata = window.btoa(username + ':' + password);
-  //                 localStorage.setItem('currentUser', JSON.stringify(user));
-  //             }
-
-  //             return user;
-  //         }));
-  // }
-
-
   login(email, password) {
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ...
-      var user = firebase.auth().currentUser;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch(function (error) {
 
-      var name, email, photoUrl, uid, emailVerified;
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ...
+      const user = firebase.auth().currentUser;
+
+
+      let name, email, photoUrl: string, uid, emailVerified;
       if (user != null) {
         name = user.displayName;
-        email = user.email;
         photoUrl = user.photoURL;
+        email = user.email;
         emailVerified = user.emailVerified;
         uid = user.uid;
 
       }
-
+      console.log(this.user);
 
     });
+    // firebase.auth().onAuthStateChanged(function (user) {
+    //   if (user) {
+    //     // User is signed in.
+    //     const displayName = user.displayName;
+    //     const email = user.email;
+    //     const emailVerified = user.emailVerified;
+    //     const photoURL = user.photoURL;
+    //     const isAnonymous = user.isAnonymous;
+    //     const uid = user.uid;
+    //     const providerData = user.providerData;
+    //     localStorage.setItem('currentUser', JSON.stringify(user));
 
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        // User is signed in.
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
-        localStorage.setItem('currentUser', JSON.stringify(user));
 
-
-        user.updateProfile({
-          displayName: "Princeton Evans",
-          photoURL: "https://icon2.kisspng.com/20180526/vcl/kisspng-picsart-photo-studio-thanos-thanos-glove-5b0a2d38b18a81.8664775915273935927272.jpg"
-        }).then(function () {
-          // Update successful.
-        }).catch(function (error) {
-          // An error happened.
-        });
-      } else {
-        // User is signed out.
-        // ...
-      }
-    });
+    //     user.updateProfile({
+    //       displayName: 'Princeton Evans',
+    //       // tslint:disable-next-line:max-line-length
+    //       photoURL: 'https://icon2.kisspng.com/20180526/vcl/kisspng-picsart-photo-studio-thanos-thanos-glove-5b0a2d38b18a81.8664775915273935927272.jpg'
+    //     }).then(function () {
+    //       // Update successful.
+    //     }).catch(function (error) {
+    //       // An error happened.
+    //     });
+    //   } else {
+    //     // User is signed out.
+    //     // ...
+    //   }
+    // });
 
   }
 
@@ -84,7 +71,7 @@ export class AuthService {
   logout() {
     // remove user from local storage to log user out
     localStorage.removeItem('currentUser');
-    this.router.navigate(['/home'])
+    this.router.navigate(['/home']);
     return  firebase.auth().signOut();
 
   }
